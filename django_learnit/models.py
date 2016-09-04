3f569a7a00ea054f38f36a66a9b163a2c02b4990
Florian PAQUET <contact@florianpaquet.com>
2016-09-04 16:47:47 +0200

    Add LabelledDocument manager

--- django_learnit/models.py
+++ django_learnit/models.py
@@ -3,6 +3,35 @@ from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 
 
+class LabelledDocumentManager(models.Manager):
+
+    def get_for_document(self, document, model_name):
+        """
+        Returns the LabelledDocument instance for the document instance
+        and the model name. Returns None if not found.
+        """
+        try:
+            return self.get_queryset().get(
+                document_content_type=ContentType.objects.get_for_model(document),
+                document_id=document.pk,
+                model_name=model_name)
+        except self.model.DoesNotExist:
+            return None
+
+    def update_or_create_for_document(self, document, model_name, value):
+        """
+        Updates or creates the LabelledDocument instance for the given
+        document and model name with the value.
+        """
+        return self.update_or_create(
+            document_content_type=ContentType.objects.get_for_model(document),
+            document_id=document.pk,
+            model_name=model_name,
+            defaults={
+                'value': value
+            })
+
+
 class LabelledDocument(models.Model):
     """
     Generic labelled document related to any
@@ -16,6 +45,8 @@ class LabelledDocument(models.Model):
     document = GenericForeignKey('document_content_type', 'document_id')
 
     value = models.TextField()
+
+    objects = LabelledDocumentManager()
 
     class Meta:
         unique_together = ('model_name', 'document_content_type', 'document_id')
--- django_learnit/tests/test_document.py
+++ django_learnit/tests/test_document.py
@@ -6,6 +6,7 @@ from django.views.generic import View
 
 from ..views.base import DocumentMixin
 from ..factories import LabelledDocumentFactory
+from ..models import LabelledDocument
 
 from .learning_models import TestModel
 from .models import Document
@@ -26,6 +27,48 @@ class LabelledDocumentModelTestCase(TransactionTestCase):
 
         self.assertTrue(
             LabelledDocumentFactory.create(model_name='model2', document=document))
+
+    def test_get_for_document_is_none_when_missing(self):
+        """Returns None when document is not labelled"""
+        document = Document.objects.create()
+        LabelledDocumentFactory.create(model_name='model', document=document)
+
+        self.assertIsNone(LabelledDocument.objects.get_for_document(document, 'other'))
+
+        document = Document.objects.create()
+        self.assertIsNone(LabelledDocument.objects.get_for_document(document, 'model'))
+
+    def test_get_for_document(self):
+        """Returns the LabelledDocument"""
+        document = Document.objects.create()
+        labelled_document = LabelledDocumentFactory.create(
+            model_name='model', document=document)
+
+        self.assertEqual(
+            LabelledDocument.objects.get_for_document(document, 'model'),
+            labelled_document)
+
+    def test_update_or_create_for_document_is_created(self):
+        """LabelledDocument is created when missing"""
+        document = Document.objects.create()
+        obj, created = LabelledDocument.objects.update_or_create_for_document(
+            document, 'model', 'foobar')
+
+        self.assertTrue(created)
+        self.assertEqual(obj.value, 'foobar')
+
+    def test_update_or_create_for_document_is_updated(self):
+        """LabelledDocument is updated when existing"""
+        document = Document.objects.create()
+        labelled_document = LabelledDocumentFactory.create(
+            document=document, model_name='model', value='oldvalue')
+
+        obj, created = LabelledDocument.objects.update_or_create_for_document(
+            document, 'model', 'newvalue')
+
+        self.assertFalse(created)
+        self.assertEqual(obj, labelled_document)
+        self.assertEqual(obj.value, 'newvalue')
 
 
 # -- Mixins
