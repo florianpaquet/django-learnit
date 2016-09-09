d612b8e0c8f0981adef136d0684cffacd4c17386
florianpaquet <contact@florianpaquet.com>
2016-09-09 16:41:18 +0200

    Use colors for classifier (wip)

--- django_learnit/learning/classifier.py
+++ django_learnit/learning/classifier.py
@@ -8,6 +8,16 @@ class GenericClassifierMixin(object):
     Classifier mixin holding available classes for the learning model
     """
     classes = ()
+    default_classes_colors = (
+        '#AB47BC',
+        '#F44336',
+        '#303F9F',
+        '#448AFF',
+        '#00796B',
+        '#8BC34A',
+        '#FBC02D',
+        '#4E342E'
+    )
 
     def get_classes(self):
         """
@@ -19,7 +29,23 @@ class GenericClassifierMixin(object):
                 'cls': self.__class__.__name__
             })
 
-        return tuple(((str(c[0]),) + c[1:]) for c in self.classes)
+        return tuple((str(c[0]), c[1]) for c in self.classes)
+
+    def get_classes_with_colors(self):
+        """
+        Returns classes with their associated colors
+        If there's no explicit color, pick one in the `default_classes_colors`
+        """
+        i = 0
+        out_classes = ()
+
+        for c in self.get_classes():
+            if len(c) != 3:
+                c += (self.default_classes_colors[i],)
+                i += 1
+            out_classes += (c,)
+
+        return out_classes
 
 
 class ClassifierModel(GenericClassifierMixin, LearningModel):
--- django_learnit/learning/ner.py
+++ django_learnit/learning/ner.py
@@ -10,28 +10,12 @@ class NamedEntityRecognizerModel(GenericClassifierMixin, LearningModel):
     outside_class_display = 'Outside'
     outside_color = '#CCCCCC'
 
-    default_colors = [
-        '#AB47BC',
-        '#F44336',
-        '#303F9F',
-        '#448AFF',
-        '#00796B',
-        '#8BC34A',
-        '#FBC02D',
-        '#4E342E'
-    ]
-
     def get_classes(self):
         """
         Returns classes with preprended oustide class
         """
-        out_classes = ()
         classes = super(NamedEntityRecognizerModel, self).get_classes()
-
-        for c in classes:
-            out_classes += (c[:2],)
-
-        return ((self.outside_class, self.outside_class_display),) + out_classes
+        return ((self.outside_class, self.outside_class_display),) + classes
 
     def get_tokens(self, document):
         """
@@ -49,16 +33,8 @@ class NamedEntityRecognizerModel(GenericClassifierMixin, LearningModel):
         Returns classes with their associated colors
         If there's no explicit color, pick one in the `default_colors` list
         """
-        i = 0
-        out_classes = ()
         classes = super(NamedEntityRecognizerModel, self).get_classes()
-
-        for c in classes:
-            if len(c) != 3:
-                c += (self.default_colors[i],)
-                i += 1
-            out_classes += (c,)
 
         return (
             (self.outside_class, self.outside_class_display, self.outside_color),
-        ) + out_classes
+        ) + classes
